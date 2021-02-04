5c4bcbf1464565fc065326d067b3e5546276d5e3
Tamaliya Das Gupta <tamaliyadasgupta9@gmail.com>
2021-02-04 15:56:20 +0000

    Basic Calculation

---
+++ session-2/basic_calculation.py
@@ -0,0 +1,49 @@
+#about addition
+	
+#assigning some value to variable x
+x=92
+#assigning some value to variable y 
+y=8
+#printing variable z to console
+z=x+y
+
+print("The result is",z)
+
+
+
+#about subtraction
+	
+#assigning some value to variable x
+x=92
+#assigning some value to variable y 
+y=8
+#printing variable z to console
+z=x-y
+
+print("The result is",z)
+
+
+
+#about multiplication
+	
+#assigning some value to variable x
+x=50
+#assigning some value to variable y 
+y=2
+#printing variable z to console
+z=x*y
+
+print("The result is",z)
+
+
+
+#about division
+	
+#assigning some value to variable x
+x=32
+#assigning some value to variable y 
+y=8
+#printing variable z to console
+z=x/y
+
+print("The result is",z)
