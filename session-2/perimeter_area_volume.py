4147652e4335b4c21061fb48b8984d41eced55cf
Tamaliya Das Gupta <tamaliyadasgupta9@gmail.com>
2021-02-04 16:04:12 +0000

    Calculating perimeter, area and volume

---
+++ session-2/perimeter_area_volume.py
@@ -0,0 +1,12 @@
+#Find the perimeter and area of a square; and volume of a cube of size 4
+
+side = 10
+
+#print perimeter
+print("Perimeter:", side*4)
+
+#print area
+print("Area:", side**2)
+
+#print volume
+print("Volume:",side**3)
