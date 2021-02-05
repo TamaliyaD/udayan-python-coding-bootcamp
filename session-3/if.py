9833d6106e97b2e882c8267db997870fd52b508c
Tamaliya Das Gupta <tamaliyadasgupta9@gmail.com>
2021-02-05 14:31:01 +0000

    If Statement

---
+++ session-3/if.py
@@ -0,0 +1,12 @@
+#Using if statement
+
+num=5
+
+if (num==5):
+    print("The number is equal to 5")
+    
+if num>5:
+    print("The number is greater than 5")
+    
+if num<5:
+    print ("The number is less than 5")
