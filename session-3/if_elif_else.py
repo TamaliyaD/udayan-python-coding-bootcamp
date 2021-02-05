0b9e8aa37abd9c573ff49d9edc6eb5a76b709443
Tamaliya Das Gupta <tamaliyadasgupta9@gmail.com>
2021-02-05 14:40:39 +0000

    if-elif-else statement

---
+++ session-3/if_elif_else.py
@@ -0,0 +1,10 @@
+#Using if_elif_else statement
+
+num = 10
+
+if num>5:
+    print ("The number is greater than 5")
+elif num%2 == 0:
+    print ("The number is even")
+else:
+    print("The number is odd and less than or equal to 5")
