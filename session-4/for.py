c7a4887279e90114050062fc3c77176b0f7a8666
Tamaliya Das Gupta <tamaliyadasgupta9@gmail.com>
2021-02-08 14:23:46 +0000

    for loop

---
+++ session-4/for.py
@@ -0,0 +1,9 @@
+for i in range(1,11): #A sequence from 1 to 10
+    if i%2 == 0:
+        print(i,"is even")
+    else:
+        print(i,"is odd")
+
+#Another example
+for i in range(1,11,3): #A sequence from 1 to 10 with a step of 3
+    print(i)
