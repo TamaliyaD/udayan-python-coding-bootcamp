642d9036b6b28a4d6037c5f79818a5bd8f612f4d
Tamaliya Das Gupta <tamaliyadasgupta9@gmail.com>
2021-02-11 06:13:47 +0000

    while statement

---
+++ session-4/while.py
@@ -0,0 +1,6 @@
+Lamp_turned_on = "True"
+
+while Lamp_turned_on == "True":
+    Lamp_turned_on = input("Please continue reading your book. Is the lamp turned on?")
+
+print("Lamp turned off! Go for sleep")
